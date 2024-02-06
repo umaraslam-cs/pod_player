c435d1a7c331cdef715acecdefc54a79f2e2f1ba
umaraslam-cs <umaraslam.cs@gmail.com>
2024-02-06 17:04:36 +0500

    text size changed

--- lib/src/widgets/core/overlays/mobile_bottomsheet.dart
+++ lib/src/widgets/core/overlays/mobile_bottomsheet.dart
@@ -187,7 +187,7 @@ class _MobileOverlayBottomControlles extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    const durationTextStyle = TextStyle(color: Colors.white70, fontSize: 12);
+    const durationTextStyle = TextStyle(color: Colors.white70, fontSize: 14);
     const itemColor = Colors.white;
 
     return GetBuilder<PodGetXVideoController>(
@@ -207,7 +207,7 @@ class _MobileOverlayBottomControlles extends StatelessWidget {
                     children: [
                       Text(
                         podCtr.calculateVideoDuration(podCtr.videoPosition),
-                        style: const TextStyle(color: itemColor, fontSize: 12),
+                        style: const TextStyle(color: itemColor, fontSize: 14),
                       ),
                       const Text(
                         ' / ',
